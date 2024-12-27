5aad217f9bf259d2a4baae765fd5d4fe9793677e
Holden Caulfield <sasha.mikheevv@gmail.com>
2024-12-27 08:59:01 +0300

    feat: change variables to prod config

--- frontend/src/config/localVariables.ts
+++ frontend/src/config/localVariables.ts
@@ -1,13 +1,15 @@
-// export enum Variables {
-//     User_URL = `https://ocean-cloud-backend.onrender.com/api/user/`,
-//     File_URL = `https://ocean-cloud-backend.onrender.com/api/`,
-//     UpAvatar_URL = `https://ocean-cloud-backend.onrender.com/api/file/avatar`,
-//     FileUpload_URL = `https://ocean-cloud-backend.onrender.com/api/file/upload`,
-// }
+const _baseUrl: string = "mellocloud.net/core";
 
-export enum Variables {
-    User_URL = `http://localhost:10000/api/user/`,
-    File_URL = `http://localhost:10000/api/`,
-    UpAvatar_URL = `http://localhost:10000/api/file/avatar`,
-    FileUpload_URL = `http://localhost:10000/api/file/upload`,
-}
+export const Variables = {
+  User_URL: `https://${_baseUrl}/api/user/`,
+  File_URL: `https://${_baseUrl}/api/`,
+  UpAvatar_URL: `https://${_baseUrl}/api/file/avatar`,
+  FileUpload_URL: `https://${_baseUrl}/api/file/upload`,
+} as const;
+
+// export enum Variables {
+//     User_URL = `http://localhost:10000/api/user/`,
+//     File_URL = `http://localhost:10000/api/`,
+//     UpAvatar_URL = `http://localhost:10000/api/file/avatar`,
+//     FileUpload_URL = `http://localhost:10000/api/file/upload`,
+// }
