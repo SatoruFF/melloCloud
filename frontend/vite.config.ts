e359640f55ddab1497799b406b84296d975e5975
Holden Caulfield <sasha.mikheevv@gmail.com>
2025-02-01 20:33:17 +0300

    feature: add rollup analyzer

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,21 @@
-import { defineConfig } from 'vite';
+import { defineConfig, type PluginOption } from 'vite';
+import { visualizer } from 'rollup-plugin-visualizer';
 import react from '@vitejs/plugin-react';
 
 // const mode = process.env.MODE || 'development';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({
+      template: 'treemap', // or sunburst
+      open: true,
+      gzipSize: true,
+      brotliSize: true,
+      filename: 'analyse.html', // will be saved in project's root
+    }) as PluginOption,
+  ],
   server: {
     fs: {
       allow: ['..'], // Разрешаем доступ к корневой папке ( для доступа к переводам например )
