e9aa4689b3372a9c9e4363cea6411ea5a93ad9e0
Holden Caulfield <sasha.mikheevv@gmail.com>
2025-02-11 20:41:19 +0300

    feature: replace node js to bun

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,7 +1,10 @@
-import config from "eslint-config-standard";
+import config from 'eslint-config-standard';
 
-
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-  ...[].concat(config),
-];
+/** @type {import('eslint').Linter.Config} */
+export default {
+  ...config, // Распаковываем конфигурацию "standard"
+  extends: [
+    ...(config.extends || []), // Добавляем существующие расширения из "standard"
+    'plugin:react-hooks/recommended', // Добавляем конфигурацию для React Hooks
+  ],
+};
