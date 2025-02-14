fbbf0ea018f8d23d1f4f3a7c53c7b88677fe6e96
Holden Caulfield <sasha.mikheevv@gmail.com>
2025-02-14 21:04:00 +0300

    feat: normalize with not found page

--- frontend/src/pages/notFoundPage/ui/NotFoundPage.tsx
+++ frontend/src/pages/notFoundPage/ui/NotFoundPage.tsx
@@ -1,11 +1,20 @@
 import { useTranslation } from 'react-i18next';
+import cn from 'classnames';
+
+import styles from './NotFoundPage.module.scss';
 
 const NotFoundPage = () => {
   const { t } = useTranslation();
 
   return (
-    <div>
-      <h1>{t('exceptions.pageNotFound')}</h1>
+    <div className={cn(styles.container)}>
+      <div className={styles.glitchContainer}>
+        <div className={styles.glitch} data-text="404">
+          404
+        </div>
+        <div className={styles.glitchLabel}>{t('exceptions.page-not-found')}</div>
+      </div>
+      s<p className={styles.message}>{t('exceptions.page-not-found-message')}</p>
     </div>
   );
 };
