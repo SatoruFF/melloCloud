be1d82bde915d13619ddd4ad7f48daeac8fcdddb
Holden Caulfield <sasha.mikheevv@gmail.com>
2025-04-09 08:37:34 +0300

    feature: add caddyfile & makefile

--- frontend/src/pages/files/lib/hooks/useFiles.ts
+++ frontend/src/pages/files/lib/hooks/useFiles.ts
@@ -1,140 +1,138 @@
-import { unwrapResult } from "@reduxjs/toolkit";
-import { message } from "antd";
-import _ from "lodash-es";
-import { useCallback, useEffect, useState } from "react";
-import { useSearchParams } from "react-router-dom";
-import { useAppDispatch, useAppSelector } from "../../../../app/store/store";
+import { unwrapResult } from '@reduxjs/toolkit';
+import { message } from 'antd';
+import _ from 'lodash-es';
+import { useCallback, useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { useAppDispatch, useAppSelector } from '../../../../app/store/store';
+import { useCreateDirMutation, useGetFilesQuery } from '../../../../entities/file/model/api/fileApi';
 import {
-	useCreateDirMutation,
-	useGetFilesQuery,
-} from "../../../../entities/file/model/api/fileApi";
+  getFilesLimitSelector,
+  getFilesOffsetSelector,
+  getFilesSelector,
+} from '../../../../entities/file/model/selectors/getFiles';
 import {
-	getFilesLimitSelector,
-	getFilesOffsetSelector,
-	getFilesSelector,
-} from "../../../../entities/file/model/selectors/getFiles";
-import {
-	addNewFile,
-	popToPath,
-	popToStack,
-	setDir,
-	setFiles,
-	setHasMore,
-	setLoading,
-	setView,
-} from "../../../../entities/file/model/slice/fileSlice";
+  addNewFile,
+  popToPath,
+  popToStack,
+  setDir,
+  setFiles,
+  setHasMore,
+  setLoading,
+  setView,
+} from '../../../../entities/file/model/slice/fileSlice';
 
 export const useFiles = () => {
-	const dispatch = useAppDispatch();
-	const currentDir = useAppSelector((state) => state.files.currentDir);
-	const dirStack = useAppSelector((state) => state.files.dirStack);
-	const paths = useAppSelector((state) => state.files.paths); // FIXME
-	const offset = useAppSelector(getFilesOffsetSelector);
-	const files = useAppSelector(getFilesSelector);
-	const limit = useAppSelector(getFilesLimitSelector);
+  const dispatch = useAppDispatch();
+  const currentDir = useAppSelector(state => state.files.currentDir);
+  const dirStack = useAppSelector(state => state.files.dirStack);
+  const paths = useAppSelector(state => state.files.paths); // FIXME
+  const offset = useAppSelector(getFilesOffsetSelector);
+  const files = useAppSelector(getFilesSelector);
+  const limit = useAppSelector(getFilesLimitSelector);
 
-	const [modal, setModal] = useState(false);
-	const [uploadModal, setUploadModal] = useState(false);
-	const [folderName, setFolderName] = useState("");
-	const [sort, setSort]: any = useState("");
-	const [search, setSearch] = useState("");
-	const [searchParams] = useSearchParams();
+  const [modal, setModal] = useState(false);
+  const [uploadModal, setUploadModal] = useState(false);
+  const [folderName, setFolderName] = useState('');
+  const [sort, setSort]: any = useState('');
+  const [search, setSearch] = useState('');
+  const [searchParams] = useSearchParams();
 
-	const onSearch = useCallback((value: string) => setSearch(value), []);
+  const onSearch = useCallback((value: string) => setSearch(value), []);
 
-	useEffect(() => {
-		// Add url parameters to the state
-		const dirFromUrl = searchParams.get("dir");
-		const sortFromUrl = searchParams.get("sort");
-		const searchFromUrl = searchParams.get("search");
+  useEffect(() => {
+    // Add url parameters to the state
+    const dirFromUrl = searchParams.get('dir');
+    const sortFromUrl = searchParams.get('sort');
+    const searchFromUrl = searchParams.get('search');
 
-		if (dirFromUrl) dispatch(setDir(dirFromUrl));
-		if (sortFromUrl) setSort(sortFromUrl);
-		if (searchFromUrl) setSearch(searchFromUrl);
-	}, []);
+    if (dirFromUrl) dispatch(setDir(dirFromUrl));
+    if (sortFromUrl) setSort(sortFromUrl);
+    if (searchFromUrl) setSearch(searchFromUrl);
+  }, []);
 
-	const params = { parent: currentDir, sort, search, offset };
-	const { data, isLoading, refetch } = useGetFilesQuery(params || null);
-	const [addFile, { data: dirData, error: dirError }] = useCreateDirMutation();
+  // TODO: destructure all params to variant like this: _.pick(obj, [...params])
+  const params = { parent: currentDir, sort, search, offset };
+  const { data, isLoading, refetch } = useGetFilesQuery(params || null);
+  const [addFile, { data: dirData, error: dirError }] = useCreateDirMutation();
 
-	useEffect(() => {
-		if (files.length == limit) {
-			dispatch(setHasMore(true));
-		} else {
-			dispatch(setHasMore(true));
-		}
-	}, [limit, offset, files, isLoading]);
+  useEffect(() => {
+    if (files.length == limit) {
+      dispatch(setHasMore(true));
+    } else {
+      dispatch(setHasMore(true));
+    }
+  }, [limit, offset, files, isLoading]);
 
-	useEffect(() => {
-		refetch();
-	}, [sort, search]);
+  useEffect(() => {
+    refetch();
+  }, [sort, search]);
 
-	useEffect(() => {
-		dispatch(setLoading(isLoading));
-	}, [isLoading]);
+  useEffect(() => {
+    dispatch(setLoading(isLoading));
+  }, [isLoading]);
 
-	useEffect(() => {
-		if (data) {
-			dispatch(setFiles(data));
-		}
-	}, [data, currentDir]);
+  useEffect(() => {
+    if (data) {
+      dispatch(setFiles(data));
+    }
+  }, [data, currentDir]);
 
-	useEffect(() => {
-		dispatch(setDir(currentDir));
-	}, [currentDir]);
+  useEffect(() => {
+    dispatch(setDir(currentDir));
+  }, [currentDir]);
 
-	useEffect(() => {
-		if (dirData) {
-			dispatch(addNewFile(dirData));
-			refetch();
-		}
-		if (dirError) {
-			message.error("Create dir error");
-		}
-	}, [dirData, dirError]);
+  useEffect(() => {
+    if (dirData) {
+      dispatch(addNewFile(dirData));
+      refetch();
+    }
+    if (dirError) {
+      message.error('Create dir error');
+    }
+  }, [dirData, dirError]);
 
-	const goBack = () => {
-		if (dirStack.length > 0) {
-			dispatch(popToStack());
-			dispatch(popToPath());
-		}
-	};
+  const goBack = () => {
+    if (dirStack.length > 0) {
+      dispatch(popToStack());
+      dispatch(popToPath());
+    }
+  };
 
-	const addNewFolder = async () => {
-		try {
-			if (folderName.length === 0) {
-				return message.info("The file name should not be empty");
-			}
-			const folderNameValid = folderName.replace(/[^\p{L}\d\s]/gu, "").trim();
-			const response: any = await addFile({
-				name: folderNameValid,
-				type: "dir",
-				parent: currentDir,
-			});
-			unwrapResult(response);
-			setModal(false);
-			setFolderName("");
-		} catch (e: any) {
-			message.error(`Request failed: ${e.data.message}`);
-		}
-	};
+  const addNewFolder = async () => {
+    try {
+      if (folderName.length === 0) {
+        return message.info('The file name should not be empty');
+      }
+      const folderNameValid = folderName.replace(/[^\p{L}\d\s]/gu, '').trim();
+      const response: any = await addFile({
+        name: folderNameValid,
+        type: 'dir',
+        parent: currentDir,
+      });
+      unwrapResult(response);
+      setModal(false);
+      setFolderName('');
+    } catch (e: any) {
+      message.error(`Request failed: ${e.data.message}`);
+    }
+  };
 
-	return {
-		modal,
-		uploadModal,
-		folderName,
-		sort,
-		search,
-		setModal,
-		setUploadModal,
-		setFolderName,
-		setSort,
-		setSearch,
-		onSearch,
-		goBack,
-		addNewFolder,
-		paths,
-		data,
-		isLoading,
-	};
+  return {
+    modal,
+    uploadModal,
+    folderName,
+    sort,
+    search,
+    setModal,
+    setUploadModal,
+    setFolderName,
+    setSort,
+    setSearch,
+    onSearch,
+    goBack,
+    addNewFolder,
+    paths,
+    data,
+    isLoading,
+  };
 };
---
+++ makefile
@@ -0,0 +1,27 @@
+up:
+	docker-compose up -d --build
+
+down:
+	docker-compose down
+
+restart:
+	docker-compose down && docker-compose up -d --build
+
+logs:
+	docker-compose logs -f
+
+clean:
+	docker system prune -af --volumes
+
+rebuild:
+	docker-compose build --no-cache
+
+restart-caddy:
+	docker-compose restart web_server
+
+# make up         # start
+# make down       # stop
+# make restart    # restert
+# make logs       # get logs
+# make clean      # clean all
+# make restart-caddy  # only caddy restart
