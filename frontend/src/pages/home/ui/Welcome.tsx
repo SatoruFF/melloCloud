ee89daa67cdfbb431cb7f4df320acf71cf10e6eb
Holden Caulfield <sasha.mikheevv@gmail.com>
2025-05-01 12:52:14 +0300

    feat: change main logo

--- frontend/src/pages/home/ui/Welcome.tsx
+++ frontend/src/pages/home/ui/Welcome.tsx
@@ -1,173 +1,137 @@
-import { Anchor, Button, message } from "antd";
-import cn from "classnames";
-import { LazyMotion, domAnimation, motion } from "framer-motion";
-import { useTranslation } from "react-i18next";
+import { Anchor, Button, message } from 'antd';
+import cn from 'classnames';
+import { LazyMotion, domAnimation, motion } from 'framer-motion';
+import { useTranslation } from 'react-i18next';
 
-import storageIcon from "../../../shared/assets/cloud-data.png";
-import desktopIcon from "../../../shared/assets/desktop.png";
-import gitIcon from "../../../shared/assets/github-icon.png";
-import mobileIcon from "../../../shared/assets/mobile-phone.png";
-import mainLogo from "../../../shared/assets/octop-main.png";
-import telegramLogo from "../../../shared/assets/telegram.png";
-import twitterLogo from "../../../shared/assets/twitter.png";
+import storageIcon from '../../../shared/assets/cloud-data.png';
+import desktopIcon from '../../../shared/assets/desktop.png';
+import gitIcon from '../../../shared/assets/github-icon.png';
+import mobileIcon from '../../../shared/assets/mobile-phone.png';
+import mainLogo from '../../../shared/assets/mello-sora.png';
+import telegramLogo from '../../../shared/assets/telegram.png';
+import twitterLogo from '../../../shared/assets/twitter.png';
 
-import ParticleEffect from "../../../shared/ui/particleEffect/ParticleEffect";
-import styles from "../styles/welcome.module.scss";
+import ParticleEffect from '../../../shared/ui/particleEffect/ParticleEffect';
+import styles from '../styles/welcome.module.scss';
 // import { useEffect } from 'react';
 
 const Welcome = () => {
-	const { t } = useTranslation();
+  const { t } = useTranslation();
 
-	return (
-		<div className={cn(styles.welcomeWrapper)}>
-			<Anchor
-				className={cn(styles.welcomeAnchor)}
-				items={[
-					{
-						key: "part-1",
-						href: "#part-1",
-						title: t("home-anchor.title"),
-					},
-					{
-						key: "part-2",
-						href: "#part-2",
-						title: t("home-anchor.more"),
-					},
-					{
-						key: "part-3",
-						href: "#part-3",
-						title: t("home-anchor.about"),
-					},
-				]}
-			/>
-			<div className={cn(styles.welcomeMain)} id="part-1">
-				<svg
-					className={cn(styles.wave)}
-					viewBox="0 0 2 1"
-					preserveAspectRatio="none"
-				>
-					<defs>
-						<path
-							id="w"
-							d="
+  return (
+    <div className={cn(styles.welcomeWrapper)}>
+      <Anchor
+        className={cn(styles.welcomeAnchor)}
+        items={[
+          {
+            key: 'part-1',
+            href: '#part-1',
+            title: t('home-anchor.title'),
+          },
+          {
+            key: 'part-2',
+            href: '#part-2',
+            title: t('home-anchor.more'),
+          },
+          {
+            key: 'part-3',
+            href: '#part-3',
+            title: t('home-anchor.about'),
+          },
+        ]}
+      />
+      <div className={cn(styles.welcomeMain)} id="part-1">
+        <svg className={cn(styles.wave)} viewBox="0 0 2 1" preserveAspectRatio="none">
+          <defs>
+            <path
+              id="w"
+              d="
             m0 1v-.5 
             q.5.5 1 0
             t1 0 1 0 1 0
             v.5z"
-						/>
-					</defs>
-					<g>
-						<use href="#w" y=".0" fill="#2d55aa" />
-						<use href="#w" y=".1" fill="#3461c1" />
-						<use href="#w" y=".2" fill="#4579e2" />
-					</g>
-				</svg>
-				<div className={cn(styles.contextMax)}>
-					<div
-						className={cn(
-							styles.welcomeCard,
-							"animate__animated animate__fadeIn",
-						)}
-					>
-						<div className={cn(styles.leftSide)}>
-							<div className={cn(styles.welcomeTitle)}>{t("primary-name")}</div>
-							<div
-								className={cn(
-									styles.welcomeDescription,
-									"animate__animated animate__fadeInDown",
-								)}
-							>
-								{t("about-product")}
-							</div>
-						</div>
-						<div className={cn(styles.rightSide)}>
-							<LazyMotion features={domAnimation}>
-								<motion.img src={mainLogo} animate={{ opacity: 1 }} />
-							</LazyMotion>
-						</div>
-					</div>
-				</div>
-			</div>
+            />
+          </defs>
+          <g>
+            <use href="#w" y=".0" fill="#2d55aa" />
+            <use href="#w" y=".1" fill="#3461c1" />
+            <use href="#w" y=".2" fill="#4579e2" />
+          </g>
+        </svg>
+        <div className={cn(styles.contextMax)}>
+          <div className={cn(styles.welcomeCard, 'animate__animated animate__fadeIn')}>
+            <div className={cn(styles.leftSide)}>
+              <div className={cn(styles.welcomeTitle)}>{t('primary-name')}</div>
+              <div className={cn(styles.welcomeDescription, 'animate__animated animate__fadeInDown')}>
+                {t('about-product')}
+              </div>
+            </div>
+            <div className={cn(styles.rightSide)}>
+              <LazyMotion features={domAnimation}>
+                <motion.img src={mainLogo} animate={{ opacity: 1 }} />
+              </LazyMotion>
+            </div>
+          </div>
+        </div>
+      </div>
 
-			{/* second content space */}
-			<div className={cn(styles.welcomeMore)} id="part-2">
-				<ParticleEffect />
-				<div className={cn(styles.moreTitle)}>{t("want-more")}</div>
-				<div className={cn(styles.contentMax)}>
-					<div className={cn(styles.moreContent)}>
-						<div className={cn(styles.moreDesktopCard)}>
-							<img src={desktopIcon} alt="desktop" loading="lazy" />
-							<div className={cn(styles.moreDescription)}>{t("desktop")}</div>
-							<Button
-								type="primary"
-								onClick={() => message.info("coming soon...")}
-							>
-								download
-							</Button>
-						</div>
+      {/* second content space */}
+      <div className={cn(styles.welcomeMore)} id="part-2">
+        <ParticleEffect />
+        <div className={cn(styles.moreTitle)}>{t('want-more')}</div>
+        <div className={cn(styles.contentMax)}>
+          <div className={cn(styles.moreContent)}>
+            <div className={cn(styles.moreDesktopCard)}>
+              <img src={desktopIcon} alt="desktop" loading="lazy" />
+              <div className={cn(styles.moreDescription)}>{t('desktop')}</div>
+              <Button type="primary" onClick={() => message.info('coming soon...')}>
+                download
+              </Button>
+            </div>
 
-						<div className={cn(styles.moreMobileCard)}>
-							<img src={mobileIcon} alt="mobile" loading="lazy" />
-							<div className={cn(styles.moreDescription)}>{t("mobile")}</div>
-							<Button type="primary">
-								<a
-									href="https://wdfiles.ru/775e318"
-									target="_blank"
-									rel="noreferrer"
-								>
-									download
-								</a>
-							</Button>
-						</div>
-					</div>
-				</div>
-			</div>
+            <div className={cn(styles.moreMobileCard)}>
+              <img src={mobileIcon} alt="mobile" loading="lazy" />
+              <div className={cn(styles.moreDescription)}>{t('mobile')}</div>
+              <Button type="primary">
+                <a href="https://wdfiles.ru/775e318" target="_blank" rel="noreferrer">
+                  download
+                </a>
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
 
-			{/* third content space */}
-			<div className={cn(styles.welcomeAbout)} id="part-3">
-				<div className={cn(styles.aboutTitle)}>{t("about-title")}</div>
-				<div className={cn(styles.contentMax)}>
-					<div className={cn(styles.aboutContent)}>
-						<div className={cn(styles.aboutDescription)}>
-							{t("about-description")}
-						</div>
-						<div className={cn(styles.aboutLinks, "aboutLinks")}>
-							<img
-								src={storageIcon}
-								alt="storage"
-								loading="lazy"
-								className={cn(styles.storageLogo)}
-							/>
-							<div className={cn(styles.socials, "socials")}>
-								<div className={cn(styles.socialsItem)}>
-									<a
-										href="https://t.me/AlexDayy"
-										target="_blank"
-										rel="noreferrer"
-									>
-										<img src={telegramLogo} alt="telegram" loading="lazy" />
-									</a>
-								</div>
-								<div className={cn(styles.socialsItem)}>
-									<a
-										href="https://github.com/SatoruFF"
-										target="_blank"
-										rel="noreferrer"
-									>
-										<img src={gitIcon} alt="instagram" loading="lazy" />
-									</a>
-								</div>
-								<div className={cn(styles.socialsItem)}>
-									<img src={twitterLogo} alt="twitter" loading="lazy" />{" "}
-								</div>
-							</div>
-							<div className={cn(styles.aboutSignature)}>Made by SatoruF</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+      {/* third content space */}
+      <div className={cn(styles.welcomeAbout)} id="part-3">
+        <div className={cn(styles.aboutTitle)}>{t('about-title')}</div>
+        <div className={cn(styles.contentMax)}>
+          <div className={cn(styles.aboutContent)}>
+            <div className={cn(styles.aboutDescription)}>{t('about-description')}</div>
+            <div className={cn(styles.aboutLinks, 'aboutLinks')}>
+              <img src={storageIcon} alt="storage" loading="lazy" className={cn(styles.storageLogo)} />
+              <div className={cn(styles.socials, 'socials')}>
+                <div className={cn(styles.socialsItem)}>
+                  <a href="https://t.me/AlexDayy" target="_blank" rel="noreferrer">
+                    <img src={telegramLogo} alt="telegram" loading="lazy" />
+                  </a>
+                </div>
+                <div className={cn(styles.socialsItem)}>
+                  <a href="https://github.com/SatoruFF" target="_blank" rel="noreferrer">
+                    <img src={gitIcon} alt="instagram" loading="lazy" />
+                  </a>
+                </div>
+                <div className={cn(styles.socialsItem)}>
+                  <img src={twitterLogo} alt="twitter" loading="lazy" />{' '}
+                </div>
+              </div>
+              <div className={cn(styles.aboutSignature)}>Made by SatoruF</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Welcome;
--- frontend/src/widgets/Navbar/ui/Navbar.tsx
+++ frontend/src/widgets/Navbar/ui/Navbar.tsx
@@ -1,184 +1,158 @@
-import {
-	ApiOutlined,
-	MenuFoldOutlined,
-	MenuUnfoldOutlined,
-	SettingOutlined,
-} from "@ant-design/icons";
-import { Button, Divider, Drawer, Tooltip, notification } from "antd";
-import cn from "classnames";
-import { motion } from "framer-motion";
-import React, { useCallback, useState } from "react";
-import { useTranslation } from "react-i18next";
-import { useMediaQuery } from "react-responsive";
-import { NavLink, useNavigate } from "react-router-dom";
+import { ApiOutlined, MenuFoldOutlined, MenuUnfoldOutlined, SettingOutlined } from '@ant-design/icons';
+import { Button, Divider, Drawer, Tooltip, notification } from 'antd';
+import cn from 'classnames';
+import { motion } from 'framer-motion';
+import React, { useCallback, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useMediaQuery } from 'react-responsive';
+import { NavLink, useNavigate } from 'react-router-dom';
 
-import { useAppDispatch, useAppSelector } from "../../../app/store/store";
-import { logout } from "../../../entities/user/model/slice/userSlice";
-import WorkspacesDropdown from "../../../features/workspaceDropdown/ui/WorkspacesDropdown";
-import avatarIcon from "../../../shared/assets/avatar-icon.png";
+import { useAppDispatch, useAppSelector } from '../../../app/store/store';
+import { logout } from '../../../entities/user/model/slice/userSlice';
+import WorkspacesDropdown from '../../../features/workspaceDropdown/ui/WorkspacesDropdown';
+import avatarIcon from '../../../shared/assets/avatar-icon.png';
 import {
-	FILE_ROUTE,
-	LOGIN_ROUTE,
-	PROFILE_ROUTE,
-	REGISTRATION_ROUTE,
-	WELCOME_ROUTE,
-} from "../../../shared/consts/routes";
-import AccountSettings from "../../accountSettings/ui/AccountSettings.";
+  FILE_ROUTE,
+  LOGIN_ROUTE,
+  PROFILE_ROUTE,
+  REGISTRATION_ROUTE,
+  WELCOME_ROUTE,
+} from '../../../shared/consts/routes';
+import AccountSettings from '../../accountSettings/ui/AccountSettings.';
 
-import { Notifications } from "../../../features/notifications";
-import mainLogo from "../../../shared/assets/octopus-kid.jpg";
-import LanguageSwitcher from "../../languageSwitcher/ui/LanguageSwitcher";
-import styles from "../styles/navbar.module.scss";
+import { Notifications } from '../../../features/notifications';
+import mainLogo from '../../../shared/assets/octopus-kid.jpg';
+import LanguageSwitcher from '../../languageSwitcher/ui/LanguageSwitcher';
+import styles from '../styles/navbar.module.scss';
 
 // TODO: add storybook
 const MyNavbar: React.FC = () => {
-	const { t } = useTranslation();
-	const isAuth = useAppSelector((state) => state.user.isAuth);
-	const user = useAppSelector((state) => state.user);
-	const [profile, setProfile] = useState(false);
-	const [burger, setBurger] = useState(false);
-	const dispatch = useAppDispatch();
-	const navigate = useNavigate();
-	const avatar = user.avatar ? user.avatar : avatarIcon;
-	const isTabletOrMobile = useMediaQuery({ query: "(max-width: 1224px)" });
+  const { t } = useTranslation();
+  const isAuth = useAppSelector(state => state.user.isAuth);
+  const user = useAppSelector(state => state.user);
+  const [profile, setProfile] = useState(false);
+  const [burger, setBurger] = useState(false);
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+  const avatar = user.avatar ? user.avatar : avatarIcon;
+  const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' });
 
-	const logOut = useCallback(() => {
-		dispatch(logout());
-		navigate(WELCOME_ROUTE);
-		notification.open({
-			message: "You succesfully log out",
-			description: "You have successfully logged out of your account",
-			placement: "topLeft",
-			icon: <ApiOutlined style={{ color: "#ff7875" }} />,
-		});
-	}, []);
+  const logOut = useCallback(() => {
+    dispatch(logout());
+    navigate(WELCOME_ROUTE);
+    notification.open({
+      message: 'You succesfully log out',
+      description: 'You have successfully logged out of your account',
+      placement: 'topLeft',
+      icon: <ApiOutlined style={{ color: '#ff7875' }} />,
+    });
+  }, []);
 
-	return (
-		<header className={cn(styles.navbar)} data-testid="navbar">
-			<div className={cn(styles.baseItems)}>
-				<LanguageSwitcher />
-				<div className={cn(styles.mainLogo)}>
-					<img src={mainLogo} alt="" onClick={() => navigate(WELCOME_ROUTE)} />
-				</div>
-			</div>
+  return (
+    <header className={cn(styles.navbar)} data-testid="navbar">
+      <div className={cn(styles.baseItems)}>
+        <LanguageSwitcher />
+        <div className={cn(styles.mainLogo)}>
+          <img src={mainLogo} alt="" onClick={() => navigate(WELCOME_ROUTE)} />
+        </div>
+      </div>
 
-			{isAuth ? (
-				<div className={cn(styles.navItems)}>
-					{!isTabletOrMobile && (
-						<React.Fragment>
-							<div className={cn(styles.navFiles)}>
-								<Button ghost>
-									<NavLink to={FILE_ROUTE}>{t("files.my-files")}</NavLink>
-								</Button>
-							</div>
-							<Button
-								className={cn(styles.mainLogout)}
-								type="primary"
-								onClick={() => logOut()}
-							>
-								{t("auth.logout")}
-							</Button>
-						</React.Fragment>
-					)}
+      {isAuth ? (
+        <div className={cn(styles.navItems)}>
+          {!isTabletOrMobile && (
+            <React.Fragment>
+              <div className={cn(styles.navFiles)}>
+                <Button ghost>
+                  <NavLink to={FILE_ROUTE}>{t('files.my-files')}</NavLink>
+                </Button>
+              </div>
+              <Button className={cn(styles.mainLogout)} type="primary" onClick={() => logOut()}>
+                {t('auth.logout')}
+              </Button>
+            </React.Fragment>
+          )}
 
-					<WorkspacesDropdown
-						setProfile={setProfile}
-						logOut={logOut}
-						viewAll={isTabletOrMobile ? true : false}
-					/>
+          <WorkspacesDropdown setProfile={setProfile} logOut={logOut} viewAll={isTabletOrMobile ? true : false} />
 
-					<div className={cn(styles.navUser)}>
-						{!isTabletOrMobile && (
-							<Tooltip title="Account Settings">
-								<div
-									className={cn(styles.userInfo)}
-									onClick={() => setProfile(true)}
-								>
-									<p>{user.userName}</p>
-									<SettingOutlined />
-								</div>
-							</Tooltip>
-						)}
-						<Notifications />
-						<div className={cn(styles.avatar)}>
-							<img src={avatar} onClick={() => navigate(PROFILE_ROUTE)} />
-						</div>
-					</div>
-					<Drawer
-						title={t("user.settings")}
-						placement="right"
-						onClose={() => setProfile(false)}
-						open={profile}
-						style={{ backgroundColor: "white" }}
-					>
-						<AccountSettings />
-					</Drawer>
-				</div>
-			) : (
-				<div className={cn(styles.navItems)}>
-					<div className={cn(styles.navItem)}>
-						<Button ghost>
-							<NavLink to={LOGIN_ROUTE}>{t("auth.authorization")}</NavLink>
-						</Button>
-					</div>
-					<div className={cn(styles.navItem)}>
-						<Button ghost>
-							<NavLink to={REGISTRATION_ROUTE}>
-								{t("auth.registration")}
-							</NavLink>
-						</Button>
-					</div>
-					<div className={cn(styles.navBurger)}>
-						{!burger ? (
-							<MenuFoldOutlined
-								className="burger-icon"
-								onClick={() => setBurger(true)}
-							/>
-						) : (
-							<MenuUnfoldOutlined className="burger-icon" />
-						)}
-						<Drawer
-							title="Pages"
-							placement="left"
-							onClose={() => setBurger(false)}
-							open={burger}
-						>
-							<motion.div
-								initial={{ opacity: 0 }}
-								animate={{ opacity: 1 }}
-								transition={{ duration: 1 }}
-								className={cn(styles.burgerItem)}
-							>
-								<Divider>
-									<NavLink to={WELCOME_ROUTE}>Home page</NavLink>
-								</Divider>
-							</motion.div>
-							<motion.div
-								initial={{ opacity: 0 }}
-								animate={{ opacity: 1 }}
-								transition={{ duration: 1 }}
-								className={cn(styles.burgerItem)}
-							>
-								<Divider>
-									<NavLink to={LOGIN_ROUTE}>Log in</NavLink>
-								</Divider>
-							</motion.div>
-							<motion.div
-								initial={{ opacity: 0 }}
-								animate={{ opacity: 1 }}
-								transition={{ duration: 1 }}
-								className={cn(styles.burgerItem)}
-							>
-								<Divider>
-									<NavLink to={REGISTRATION_ROUTE}>Registration</NavLink>
-								</Divider>
-							</motion.div>
-						</Drawer>
-					</div>
-				</div>
-			)}
-		</header>
-	);
+          <div className={cn(styles.navUser)}>
+            {!isTabletOrMobile && (
+              <Tooltip title="Account Settings">
+                <div className={cn(styles.userInfo)} onClick={() => setProfile(true)}>
+                  <p>{user.userName}</p>
+                  <SettingOutlined />
+                </div>
+              </Tooltip>
+            )}
+            <Notifications />
+            <div className={cn(styles.avatar)}>
+              <img src={avatar} onClick={() => navigate(PROFILE_ROUTE)} />
+            </div>
+          </div>
+          <Drawer
+            title={t('user.settings')}
+            placement="right"
+            onClose={() => setProfile(false)}
+            open={profile}
+            style={{ backgroundColor: 'white' }}
+          >
+            <AccountSettings />
+          </Drawer>
+        </div>
+      ) : (
+        <div className={cn(styles.navItems)}>
+          <div className={cn(styles.navItem)}>
+            <Button ghost>
+              <NavLink to={LOGIN_ROUTE}>{t('auth.authorization')}</NavLink>
+            </Button>
+          </div>
+          <div className={cn(styles.navItem)}>
+            <Button ghost>
+              <NavLink to={REGISTRATION_ROUTE}>{t('auth.registration')}</NavLink>
+            </Button>
+          </div>
+          <div className={cn(styles.navBurger)}>
+            {!burger ? (
+              <MenuFoldOutlined className="burger-icon" onClick={() => setBurger(true)} />
+            ) : (
+              <MenuUnfoldOutlined className="burger-icon" />
+            )}
+            <Drawer title="Pages" placement="left" onClose={() => setBurger(false)} open={burger}>
+              <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 1 }}
+                className={cn(styles.burgerItem)}
+              >
+                <Divider>
+                  <NavLink to={WELCOME_ROUTE}>Home page</NavLink>
+                </Divider>
+              </motion.div>
+              <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 1 }}
+                className={cn(styles.burgerItem)}
+              >
+                <Divider>
+                  <NavLink to={LOGIN_ROUTE}>Log in</NavLink>
+                </Divider>
+              </motion.div>
+              <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 1 }}
+                className={cn(styles.burgerItem)}
+              >
+                <Divider>
+                  <NavLink to={REGISTRATION_ROUTE}>Registration</NavLink>
+                </Divider>
+              </motion.div>
+            </Drawer>
+          </div>
+        </div>
+      )}
+    </header>
+  );
 };
 
 export default MyNavbar;
