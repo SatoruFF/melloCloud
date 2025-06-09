654ebd59c9c36385e3c72e1b7391a5731322a131
Holden Caulfield <sasha.mikheevv@gmail.com>
2025-06-10 02:19:54 +0300

    Add editor component styles and update Notes layout

--- frontend/src/pages/notes/ui/Notes.tsx
+++ frontend/src/pages/notes/ui/Notes.tsx
@@ -5,12 +5,16 @@ import NotesSidebar from "../../../widgets/notesSidebar/ui/NotesSidebar";
 import styles from "./notes.module.scss";
 
 const Notes = () => {
-	return (
-		<div className={cn(styles.notesWrapper)}>
-			<NotesSidebar />
-			<Editor />
-		</div>
-	);
+  return (
+    <div className={cn(styles.notesWrapper)}>
+      <div className={styles.sidebar}>
+        <NotesSidebar />
+      </div>
+      <div className={styles.editor}>
+        <Editor />
+      </div>
+    </div>
+  );
 };
 
 export default Notes;
--- frontend/src/widgets/editor/ui/Editor.tsx
+++ frontend/src/widgets/editor/ui/Editor.tsx
@@ -1,5 +1,19 @@
-const EditorWithDrawing = () => {
-	return <></>;
-};
+import "@blocknote/core/fonts/inter.css";
+import { BlockNoteView } from "@blocknote/mantine";
+import "@blocknote/mantine/style.css";
+import { useCreateBlockNote } from "@blocknote/react";
 
-export default EditorWithDrawing;
+import styles from "./editor.module.scss";
+import "./editor.scss";
+
+export default function App() {
+  const editor = useCreateBlockNote();
+
+  return (
+    <div className={styles.editorWrapper}>
+      <div className={styles.blocknoteContainer}>
+        <BlockNoteView className={styles.editor} editor={editor} />
+      </div>
+    </div>
+  );
+}
